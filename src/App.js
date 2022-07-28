a593156d7bf7fed3f28dcf7da0d0f44eae74cd65
unknown <ksk13minecraft@gmail.com>
2022-07-28 06:45:12 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import {
 } from "react-router-dom";
 import GraphPage from './screens/GraphPage/GraphPage';
 import AddTemplate from './screens/AddTemplate/AddTemplate';
-import Customers from './screens/Customers.js/Customers';
+import Customers from './screens/Customers/Customers';
+import UploadFile from './screens/UploadFile/UploadFile';
 
 
 
@@ -25,6 +26,7 @@ function App() {
           <Route path='/analytics' element={<GraphPage />}/>
           <Route path='/add-template' element={<AddTemplate />}/>
           <Route path='/customers' element={<Customers />} />
+          <Route path='/upload' element={<UploadFile />} />
       </Routes>
       </Router>
     </>
---
+++ src/screens/UploadFile/UploadFile.js
@@ -0,0 +1,19 @@
+import { Box } from '@mui/system'
+import React from 'react'
+import BasePage from '../../MiscComponents/BasePage'
+export default function UploadFile() {
+    return (
+        <>
+            <BasePage>
+                <Box style={{backgroundColor:'white', width:'90%', height:"70vh", padding:'1.5rem 1.5rem', margin:"2.5rem auto"}}>
+                    <Box style={{fontSize:'1.5rem', color:'#4D4F5C'}}>
+                        Upload
+                    </Box>
+                    <Box>
+
+                    </Box>
+                </Box>
+            </BasePage>
+        </>
+    )
+}
